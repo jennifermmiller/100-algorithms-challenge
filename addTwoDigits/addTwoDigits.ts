48f915380d9b5e9344a7545b9b269986c4880ee3
Jennifer Miller <jmiller6128@gmail.com>
2021-03-10 11:32:11 -0500

    addTwoDigits problem

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,18 @@
 export function addTwoDigits(n: any): number {
+  const splitNumber = n.toString().split('');
 
+  const sum = splitNumber.reduce((acc:string, cur:string): number => parseInt(acc, 10) + parseInt(cur, 10));
+
+  return sum;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+
+
+// Problem clearly says two digits. Solved above for any number of digits
+// Alternative:
+// export function addTwoDigits(n: any): number {
+//   const splitNumber = n.toString().split('');
+
+//   return parseInt(splitNumber[0], 10) + parseInt(splitNumer[1], 10));
+// }
