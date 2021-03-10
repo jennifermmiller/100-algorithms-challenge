827dcfad00c57c1221d9a615a46e3057709ef638
Jennifer Miller <jmiller6128@gmail.com>
2021-03-10 10:54:18 -0500

    absoluteSumMinization problem

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,20 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    // if even, divide length by 2 and substract one (for index)
+    // if odd, divide  length by 2 and round down
+    const median = a.length%2 === 0 ? a.length/2 - 1 : Math.floor(a.length/2) ;
+    
+    return a[median];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+
+
+// Alternative:
+// export function absoluteValuesSumMinimization(a: number[]): number {
+//     const isEven = a.length%2 === 0;
+    
+//     return isEven ? a[a.length/2 - 1] : a[Math.floor(a.length/2)];
+// }
